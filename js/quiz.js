8f3133c67ed1dcf23315768d624d5d92235e56b6
CrwnChakra <devdevkev1@gmail.com>
2022-08-05 12:29:38 -0500

    refactored js

--- js/quiz.js
+++ js/quiz.js
@@ -1,32 +1,29 @@
 export default function Quiz(questions) {
     this.questions = questions;
-    this.score = 0; 
-    this.currentIndex = 0;             // need position of current question
+    this.score = 0;
+    this.currentIndex = 0; // need position of current question
 }
-
-Quiz.prototype.getCurrentQuestion = function() {
-    return this.questions[this.currentIndex]
+Quiz.prototype.getCurrentQuestion = function () {
+    return this.questions[this.currentIndex];
 }
 // increments every index
-Quiz.prototype.nextIndex = function() {     
-    this.currentIndex++ 
+Quiz.prototype.nextIndex = function () {
+    this.currentIndex++;
 }
 // check if game ended | currentIndex === Length
-Quiz.prototype.hasEnded = function() {      
-    return this.currentIndex === this.questions.length()
+Quiz.prototype.hasEnded = function () {
+    return this.currentIndex === this.questions.length;
 }
 // guessing the answer. Compares current answer key to userGuess
-Quiz.prototype.guess = function(userGuess) {
-    const currentQuestion = this.getCurrentQuestion();
+Quiz.prototype.guess = function (userGuess) {
+    const currentQuestion = this.questions[this.currentIndex];
     // take in user input and checking with userGuess
     if (currentQuestion.isCorrect(userGuess)) {
         this.score++;
     }
-
     this.nextIndex();
 }
-
-Quiz.prototype.reset = function() {
+Quiz.prototype.reset = function () {
     this.score = 0;
     this.currentIndex = 0;
 }
